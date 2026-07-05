b86dc3fe919b33d1a97ad3c54458c0c3c63b61d0
DESKTOP-1B2D14T\Grabovsky <andovgopolyy@gmail.com>
2026-07-05 18:05:19 +0300

    logger

--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -1,6 +1,49 @@
 #include <Logger/Logger.h>
 
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/ringbuffer_sink.h>
+
 namespace shen3
 {
+    Logger::Logger()
+    {
+        constexpr size_t buffer_size = 8192;
+        auto ringbufferSink = std::make_shared<spdlog::sinks::ringbuffer_sink_mt>(buffer_size);
+        auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        auto fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt", true); // TODO filepath
 
+        _logger = std::make_unique<spdlog::logger>("logger", spdlog::sinks_init_list{ consoleSink, fileSink, ringbufferSink });
+        _logger->set_pattern("[%Y-%m-%d %H:%M:%S.%f %z] [%^%l%$] %v");
+    }
+
+    Logger& Logger::Instance()
+    {
+        static Logger inst;
+        return inst;
+    }
+
+    void Logger::Log(const std::string& msg)
+    {
+        Instance().Log("{}", msg);
+    }
+
+    void Logger::Warn(const std::string& msg)
+    {
+        Instance().Warn("{}", msg);
+    }
+
+    void Logger::Err(const std::string& msg)
+    {
+        Instance().Err("{}", msg);
+    }
 }
+
--- src/Logger/Logger.h
+++ src/Logger/Logger.h
@@ -1,9 +1,60 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include <memory>
+
+#include <spdlog/spdlog.h>
+
 namespace shen3
 {
+    enum class LogType
+    {
+        Info,
+        Warning,
+        Error
+    };
+
+    struct LogEntry
+    {
+        LogType type;
+        std::string message;
+    };
+
     class Logger
     {
+    public:
+        static Logger& Instance();
 
+        static void Log(const std::string& msg);
+        static void Warn(const std::string& msg);
+        static void Err(const std::string& msg);
+
+        template<typename... Args>
+        static void Log(const std::string& format, Args&& ...args)
+        {
+            Instance().GetLogger()->info(std::vformat(format, std::make_format_args(std::forward<Args>(args)...)));
+        }
+
+        template<typename... Args>
+        static void Warn(const std::string& format, Args&& ...args)
+        {
+            Instance().GetLogger()->warn(std::vformat(format, std::make_format_args(std::forward<Args>(args)...)));
+        }
+
+        template<typename... Args>
+        static void Err(const std::string& format, Args&& ...args)
+        {
+            Instance().GetLogger()->error(std::vformat(format, std::make_format_args(std::forward<Args>(args)...)));
+        }
+
+    private:
+        Logger();
+
+        spdlog::logger* GetLogger() const { return _logger.get(); }
+
+    private:
+        std::unique_ptr<spdlog::logger> _logger;
+        std::vector<LogEntry> _messages;
     };
 }
